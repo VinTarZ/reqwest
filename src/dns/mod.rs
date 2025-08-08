b23598e20f5a60849f04fdb8d7fd39fb58f72650
VinTarZ <vintarz@yandex.ru>
2025-08-08 19:14:42 +0300

    Reexport hickory_resolver

--- src/dns/mod.rs
+++ src/dns/mod.rs
@@ -3,6 +3,9 @@
 pub use resolve::{Addrs, Name, Resolve, Resolving};
 pub(crate) use resolve::{DnsResolverWithOverrides, DynResolver};
 
+#[cfg(feature = "hickory-dns")]
+pub use hickory_resolver;
+
 pub(crate) mod gai;
 #[cfg(feature = "hickory-dns")]
 pub(crate) mod hickory;
